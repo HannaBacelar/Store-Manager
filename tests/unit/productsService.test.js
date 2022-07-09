108a1f237b58654f92face7fc314840a48993457
Hanna Bacelar <hannabacelar@hotmail.com>
2022-07-08 21:03:19 -0300

    :lipstick: feat: testando a função getAll do service

--- tests/unit/productsService.test.js
+++ tests/unit/productsService.test.js
@@ -1,24 +1,42 @@
-// const sinon = require('sinon');
-// const { expect } = require('chai');
+const sinon = require('sinon');
+const { expect } = require('chai');
 
-// const productService = require('../../models/productService');
-// const productModel = require('../../models/productModel');
+const productService = require('../../services/productService');
+const productModel = require('../../models/productModel');
 
-// const connection = require('../../helpers/connection');
+const connection = require('../../helpers/connection');
 
-// const products = [{
-//     id: 1,
-//     name: 'Martelo de Thor',
-//   },
-//   {
-//     id: 2,
-//     name: 'Traje de encolhimento',
-//   },
-// ];
+const products = [{
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+  },
+];
 
-// const product = [
-//   {
-//     id: 1,
-//     name: 'Martelo de Thor',
-//   },
-// ];
+const product = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+];
+
+describe('testa aqruivo Services', () => {
+  after(() => {
+    sinon.restore();
+  });
+
+  describe('testa função getAll', () => {
+    it('verifica se é possível retornar um array de objetos se houver produtos', async () => {
+      sinon.stub(productModel, 'getAll').resolves(products);
+
+      const result = await productService.getAll();
+
+      expect(result).to.be.an('array');
+
+      products.forEach((product) => expect(product).to.be.an('object'));
+    });
+  });
+  });
